5cce857cf47b511b753bcba31b9ef01cadf3674c
francisperes <fm.peres@campus.fct.unl.pt>
2019-01-21 14:24:56 +0000

    First commit

--- main.go
+++ main.go
@@ -1,45 +1,85 @@
 package main
 
 import (
-"database/sql"
-"fmt"
-"strconv"
+	"fmt"
+	"time"
 
-_ "github.com/mattn/go-sqlite3"
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
 )
 
+type Alunos struct {
+	ID        int `gorm:"AUTO_INCREMENT"`
+	FirstName string
+	LastName  string
+	DegreeID  int
+	StartDate time.Time
+}
+
 func main() {
+	db, err := gorm.Open("sqlite3", "clip_holy_grail.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	db.SingularTable(true)
+	if !db.HasTable(Alunos{}) {
+		err = db.CreateTable(Alunos{}).Error
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	newAluno := Alunos{FirstName: "John", LastName: "Doe", DegreeID: 1, StartDate: time.Now()}
+	err = db.Save(&newAluno).Error
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var alunos []Alunos
+	err = db.Find(&alunos).Error
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(alunos)
+}
+
+/*func main() {
 	database, err := sql.Open("sqlite3", "./clip_holy_grail.db")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	statement, err := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
+	statement, err := database.Prepare("CREATE TABLE IF NOT EXISTS alunos (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT, curso_id NUMBER, start_date DATE)")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	statement.Exec()
-	statement, err = database.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
+	statement, err = database.Prepare("INSERT INTO alunos (firstname, lastname, curso_id) VALUES (?, ?, ?)")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	statement.Exec("John", "Doe")
-	rows, err := database.Query("SELECT id, firstname, lastname FROM people")
+	statement.Exec("Francisco", "Peres", 50034)
+	rows, err := database.Query("SELECT id, firstname, lastname, curso_id FROM alunos")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	
-	var id int
+
+	var id, cursoID int
 	var firstname, lastname string
 	for rows.Next() {
-		rows.Scan(&id, &firstname, &lastname)
-		fmt.Println(strconv.Itoa(id) + ": " + firstname + " " + lastname)
+		rows.Scan(&id, &firstname, &lastname, &cursoID)
+		fmt.Println(strconv.Itoa(id) + ": " + firstname + " " + lastname + " " + strconv.Itoa(cursoID))
 	}
-}
-
+}*/
