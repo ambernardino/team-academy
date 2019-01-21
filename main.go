1b5b8624d3f2222ab36b28e128df4a746a06f2fd
pmsdoliveira <pmsd.oliveira@campus.fct.unl.pt>
2019-01-21 17:45:44 +0000

    Removed comments

--- main.go
+++ main.go
@@ -149,38 +149,3 @@ func main() {
 	}
 	fmt.Println(studentSubject)
 }
-
-/*func main() {
-	/*database, err := sql.Open("sqlite3", "./clip_holy_grail.db")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	statement, err := database.Prepare("CREATE TABLE IF NOT EXISTS alunos (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT, curso_id NUMBER, start_date DATE)")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	statement.Exec()
-	statement, err = database.Prepare("INSERT INTO alunos (firstname, lastname, curso_id) VALUES (?, ?, ?)")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	statement.Exec("Pedro", "Oliveira", "50544")
-	rows, err := database.Query("SELECT id, firstname, lastname, curso_id FROM alunos")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	var id, curso_id int
-	var firstname, lastname string
-	for rows.Next() {
-		rows.Scan(&id, &firstname, &lastname, &curso_id)
-		fmt.Println(strconv.Itoa(id) + ": " + firstname + " " + lastname + " " + strconv.Itoa(curso_id))
-	}
-}*/
