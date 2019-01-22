e32ec1dbcd903fc79e0fe54105c36252f3e6abc5
pmsdoliveira <pmsd.oliveira@campus.fct.unl.pt>
2019-01-22 17:18:09 +0000

    Add/visualize grades

--- grades/db.go
+++ grades/db.go
@@ -1,7 +1,8 @@
 package grades
 
 import (
-	"team-academy/subject"
+	"fmt"
+	"team-academy/student_subject"
 
 	"github.com/jinzhu/gorm"
 )
@@ -12,14 +13,39 @@ type Grade struct {
 	Rank      string
 }
 
-func GiveGrade(db *gorm.DB, grade Grade) (err error) {
-	err = subject.GetSubjectByID(db, grade.SubjectID)
-	if err != nil {
+type StudentGrade struct {
+	StudentID        int
+	StudentFirstName string `gorm:"column:first_name"`
+	StudentLastName  string `gorm:"column:last_name"`
+	SubjectName      string `gorm:"column:name"`
+	SubjectID        int
+}
 
+func GetStudentsGrades(db *gorm.DB) (grades []StudentGrade, err error) {
+	err = db.Table("student").Select("student_subject.student_id, student.first_name, student.last_name, subject.name, student_subject.subject_id").Joins("JOIN student_subject ON student.id = student_subject.student_id").Joins("JOIN subject on subject.id = student_subject.subject_id").Scan(&grades).Error
+	for _, v := range grades {
+		fmt.Println(v)
 	}
+
+	return
+}
+
+func GiveGrade(db *gorm.DB, grade Grade) (err error) {
+	_, err = db.Table("student").Select("student.id, student_subject.subject_id").Joins("JOIN student_subject ON student_subject.student_id = student.id").Where(&student_subject.StudentSubject{SubjectID: grade.SubjectID}).Rows()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	return db.Save(&grade).Error
 }
 
-func UpdateGrade(db *gorm.DB, grade Grade) (err error) {
+func GetGradeByStudentIDAndSubjectID(db *gorm.DB, studentID int, subjectID int) (grade Grade, err error) {
+	err = db.First(&grade, &Grade{StudentID: studentID, SubjectID: subjectID}).Error
+	return
+}
 
+func UpdateGrade(db *gorm.DB, grade Grade) (err error) {
+	return nil
 }
--- student/db.go
+++ student/db.go
@@ -35,6 +35,11 @@ func DeleteStudent(db *gorm.DB, id int) (err error) {
 	return db.Delete(&Student{ID: id}).Error
 }
 
+func GetStudentByID(db *gorm.DB, id int) (student Student, err error) {
+	err = db.First(&student, &Student{ID: id}).Error
+	return
+}
+
 func GetAllStudents(db *gorm.DB) (students []Student, err error) {
 	err = db.Find(&students).Error
 	return
