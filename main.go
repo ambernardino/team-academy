9eced99a8b223ca821392a7991dd383edb2b225f
tfcmarques98 <tfcmarques98@gmail.com>
2019-01-28 10:35:16 +0000

    Fixed some issues with a duplicated file

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"team-academy/student"
 	"team-academy/student_subject"
 	"team-academy/subject"
+	"time"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/mattn/go-sqlite3"
@@ -17,84 +18,97 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-
 	db.SingularTable(true)
-
-	err = professor.CreateTableIfNotExists(db)
+	err = populateDatabase(db)
 	if err != nil {
 		fmt.Println(err)
+		return
+	}
+}
+
+func populateDatabase(db *gorm.DB) (err error) {
+	err = professor.CreateTableIfNotExists(db)
+	if err != nil {
 		return
 	}
 
 	err = student.CreateTableIfNotExists(db)
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
 
 	err = subject.CreateTableIfNotExists(db)
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
 
 	err = student_subject.CreateTableIfNotExists(db)
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
 
-
-	newSubject := subject.Subject{ID: 2, Name: "Eletrónica 1", Description: "Uma seca desgraçada"}
+	newSubject := subject.Subject{ID: 1, Name: "Cadeira 1", Description: "Nothing"}
 	err = subject.CreateSubject(db, newSubject)
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
 
-	err = student_subject.AddStudentToSubject(db, 1, 1)
+	newSubject = subject.Subject{ID: 2, Name: "Cadeira 2", Description: "Nothing"}
+	err = subject.CreateSubject(db, newSubject)
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
 
-	updatedSubject := subject.Subject{ID: 2, Name: "Eletrónica 2", Description: "Outra seca desgraçada"}
-	err = subject.UpdateSubjectInfo(db, updatedSubject)
+	newSubject = subject.Subject{ID: 3, Name: "Cadeira 3", Description: "Nothing"}
+	err = subject.CreateSubject(db, newSubject)
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
 
-	err = subject.DeleteSubject(db, 8)
+	newProfessor := professor.Professor{ID: 1, FirstName: "Prof 1", LastName: "Prof 1", CursoIDs: "Curso 1", CadeiraIDS: "Cadeira 1", StartDate: time.Now().UTC()}
+	err = professor.CreateProfessor(db, newProfessor)
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
 
-	err = student_subject.RemoveStudentFromSubject(db, 1, 1)
+	newProfessor = professor.Professor{ID: 2, FirstName: "Prof 2", LastName: "Prof 2", CursoIDs: "Curso 2", CadeiraIDS: "Cadeira 2", StartDate: time.Now().UTC()}
+	err = professor.CreateProfessor(db, newProfessor)
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
 
-	subjects, err := subject.GetAllSubjects(db)
+	newProfessor = professor.Professor{ID: 3, FirstName: "Prof 3", LastName: "Prof 3", CursoIDs: "Curso 3", CadeiraIDS: "Cadeira 3", StartDate: time.Now().UTC()}
+	err = professor.CreateProfessor(db, newProfessor)
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
-	fmt.Println(subjects)
 
-	studentSubject, err := student_subject.GetSubjectsByStudentID(db, 1)
+	newStudent := student.Student{ID: 1, FirstName: "Student 1", LastName: "Student 1", CursoID: 1, StartDate: time.Now().UTC()}
+	err = student.CreateStudent(db, newStudent)
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
-	fmt.Println(studentSubject)
 
-	studentSubject, err = student_subject.GetSubjectsByStudentID(db, 1)
+	newStudent = student.Student{ID: 2, FirstName: "Student 2", LastName: "Student 2", CursoID: 2, StartDate: time.Now().UTC()}
+	err = student.CreateStudent(db, newStudent)
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
-	fmt.Println(studentSubject)
+
+	newStudent = student.Student{ID: 3, FirstName: "Student 3", LastName: "Student 3", CursoID: 3, StartDate: time.Now().UTC()}
+	err = student.CreateStudent(db, newStudent)
+	if err != nil {
+		return
+	}
+
+	for i := 1; i <= 3; i++ {
+		for j := 1; j <= 3; j++ {
+			err = student_subject.AddStudentToSubject(db, i, j)
+			if err != nil {
+				return
+			}
+		}
+	}
+
+	return
 }
