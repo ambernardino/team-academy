9c8fc54033fb31bffd5748f469643ea45e1b3568
Antonio Bernardino <am.bernardino@outlook.com>
2019-02-28 13:22:11 +0000

    Updated docker-compose scripts
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   Gopkg.lock
    	modified:   Gopkg.toml
    	modified:   deploy.sh
    	modified:   docker-compose.yml

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 dep ensure
 CGO_ENABLED=1 GOOS=linux go build -a -installsuffix cgo -o main main.go
+sudo /root/bin/docker-compose down
 sudo docker build . -t clip-go-image
 sudo docker save clip-go-image -o docker_image
-sudo rm docker-image
+sudo /root/bin/docker-compose up -d
+sudo rm docker_image
+
